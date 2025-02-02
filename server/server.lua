bbdcc444c6774fe7bb12e8cc6ccc0ffd6ac152a8
Pingu <98775320+Alokas123@users.noreply.github.com>
2025-02-02 14:15:35 +0200

    Update server.lua
    check when giving drugs that player has same id as the starter of mission.

--- server/server.lua
+++ server/server.lua
@@ -1,20 +1,19 @@
-lib.locale() -- Initialize locale system
 local ConfigServer = require 'config/sv_config'
 local lastPickupCoord = nil
 local hasStarted = false
 local cooldownEndTime = 0
+local missionStarter = nil  
 
 local function isCooldownActive()
     return os.time() < cooldownEndTime
 end
-
 
 lib.callback.register('location', function()
     return ConfigServer.StartLocation.coords, ConfigServer.StartLocation.heading
 end)
 
 lib.callback.register('givepacket', function(source)
-    if hasStarted then
+    if hasStarted and source == missionStarter then  -- Now correctly checking if source is the mission starter
         exports.ox_inventory:AddItem(source, ConfigServer.PacketItem, 1)
         return true
     end
@@ -30,7 +29,8 @@ lib.callback.register('Missionstart', function(source, phonenumber)
 
     if money >= ConfigServer.MissionCost then
         hasStarted = true
-        cooldownEndTime = os.time() + (ConfigServer.CoolDown * 60) 
+        missionStarter = source 
+        cooldownEndTime = os.time() + (ConfigServer.CoolDown * 60)
 
         lastPickupCoord = ConfigServer.PickupLocations.coords[math.random(1, #ConfigServer.PickupLocations.coords)]
         local message = ConfigServer.PickupLocations.messages[math.random(1, #ConfigServer.PickupLocations.messages)]
